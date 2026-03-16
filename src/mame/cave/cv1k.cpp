219c614ba5ca36f9db8e4d54c13031c2d805fd60
jmvannieu <jmvannieu@gmail.com>
2026-03-15 20:44:04 -0500

    Added the always true if condition

--- src/mame/cave/cv1k.cpp
+++ src/mame/cave/cv1k.cpp
@@ -162,17 +162,10 @@ Common game codes:
    (EEPROM or NAND settings area)
  - During boot hold P1 A and P2 A - Reset random numbers generator at each game start.
    Probably was used during testing or/and competition events.
- - During boot hold P1 Right+A+B+C and P2 Left+A+B+C - Forcibly initialise non-volatile data
-   (EEPROM or NAND settings area)
- - During boot hold P1 A and P2 A - Reset random numbers generator at each game start.
-   Probably was used during testing or/and competition events.
 
 TODO:
 
 Improve Blending precision?
- - I'm not sure what precision the original HW mixes with, source data is 555 RGB with 1 bit
-   transparency (16-bits) and the real VRAM is also clearly in this format.  The Alpha values
-   supplied however are 8bpp, and the 'Tint' values use 0x20 for 'normal' (not 0x1f)
  - I'm not sure what precision the original HW mixes with, source data is 555 RGB with 1 bit
    transparency (16-bits) and the real VRAM is also clearly in this format.  The Alpha values
    supplied however are 8bpp, and the 'Tint' values use 0x20 for 'normal' (not 0x1f)
@@ -909,32 +902,34 @@ ROM_START( dfkbl )
 	ROM_LOAD16_WORD_SWAP( "u24", 0x400000, 0x400000, CRC(31f9eb0a) SHA1(322158779e969bb321241065dd49c1167b91ff6c) )
 ROM_END
 
+#if 1 // now included in MAME
 ROM_START( akatana )
-   ROM_REGION( 0x400000, "maincpu", ROMREGION_ERASEFF)
-   ROM_LOAD16_WORD_SWAP( "u4", 0x000000, 0x400000, CRC(613fd380) SHA1(6e28480eef3b483d00b42d811a9d2c7fa1097924) ) // (2010/ 8/13 MASTER VER.)
+	ROM_REGION( 0x400000, "maincpu", ROMREGION_ERASEFF)
+	ROM_LOAD16_WORD_SWAP( "u4", 0x000000, 0x400000, CRC(613fd380) SHA1(6e28480eef3b483d00b42d811a9d2c7fa1097924) ) // (2010/ 8/13 MASTER VER.)
 
-   ROM_REGION( 0x8400000, "nand", ROMREGION_ERASEFF)
-   ROM_LOAD( "u2", 0x000000, 0x8400000, CRC(89a2e1a5) SHA1(e6f4ec974406283665697fdd52bd606d0337dd11) )
+	ROM_REGION( 0x8400000, "nand", ROMREGION_ERASEFF)
+	ROM_LOAD( "u2", 0x000000, 0x8400000, CRC(89a2e1a5) SHA1(e6f4ec974406283665697fdd52bd606d0337dd11) )
 
-   ROM_REGION( 0x800000, "ymz770", ROMREGION_ERASEFF)
-   ROM_LOAD16_WORD_SWAP( "u23", 0x000000, 0x400000, CRC(34a67e24) SHA1(78a7e82123b86311f1116a80c39f147b8b695549) )
-   ROM_LOAD16_WORD_SWAP( "u24", 0x400000, 0x400000, CRC(10760fed) SHA1(b70f4506c00f3901ff38f5efd4b897af1afc7a0c) )
+	ROM_REGION( 0x800000, "ymz770", ROMREGION_ERASEFF)
+	ROM_LOAD16_WORD_SWAP( "u23", 0x000000, 0x400000, CRC(34a67e24) SHA1(78a7e82123b86311f1116a80c39f147b8b695549) )
+	ROM_LOAD16_WORD_SWAP( "u24", 0x400000, 0x400000, CRC(10760fed) SHA1(b70f4506c00f3901ff38f5efd4b897af1afc7a0c) )
 ROM_END
 
 ROM_START( ddpsdoj )
-   ROM_REGION( 0x400000, "maincpu", ROMREGION_ERASEFF)
-   ROM_LOAD16_WORD_SWAP("u4", 0x000000, 0x400000, CRC(e2a4411c) SHA1(f8b2b6326dd8eeae99b8b1ab2bd5a3f0b9c7f027) )
-   ROM_IGNORE( 0x000100 ) // Flash extra bytes
+	ROM_REGION( 0x400000, "maincpu", ROMREGION_ERASEFF)
+	ROM_LOAD16_WORD_SWAP("u4", 0x000000, 0x400000, CRC(e2a4411c) SHA1(f8b2b6326dd8eeae99b8b1ab2bd5a3f0b9c7f027) )
+	ROM_IGNORE( 0x000100 ) // Flash extra bytes
 
-   ROM_REGION( 0x8400000, "nand", ROMREGION_ERASEFF)
-   ROM_LOAD("u2", 0x000000, 0x8400000, CRC(668e4cd6) SHA1(da0b10865df3a3e46cf8a109ca88a551faba4483) )
+	ROM_REGION( 0x8400000, "nand", ROMREGION_ERASEFF)
+	ROM_LOAD("u2", 0x000000, 0x8400000, CRC(668e4cd6) SHA1(da0b10865df3a3e46cf8a109ca88a551faba4483) )
 
-   ROM_REGION( 0x800000, "ymz770", ROMREGION_ERASEFF)
-   ROM_LOAD16_WORD_SWAP("u23", 0x000000, 0x400000, CRC(ac94801c) SHA1(cbcc6d5d89860bc961967e1d3b7c329adaf200c5) )
-   ROM_IGNORE( 0x000100 ) // Flash extra bytes
-   ROM_LOAD16_WORD_SWAP("u24", 0x400000, 0x400000, CRC(f593045b) SHA1(91b92870d0dd2a7817cb0059cc750e2393686f4c) )
-   ROM_IGNORE( 0x000100 ) // Flash extra bytes
+	ROM_REGION( 0x800000, "ymz770", ROMREGION_ERASEFF)
+	ROM_LOAD16_WORD_SWAP("u23", 0x000000, 0x400000, CRC(ac94801c) SHA1(cbcc6d5d89860bc961967e1d3b7c329adaf200c5) )
+	ROM_IGNORE( 0x000100 ) // Flash extra bytes
+	ROM_LOAD16_WORD_SWAP("u24", 0x400000, 0x400000, CRC(f593045b) SHA1(91b92870d0dd2a7817cb0059cc750e2393686f4c) )
+	ROM_IGNORE( 0x000100 ) // Flash extra bytes
 ROM_END
+#endif
 
 u64 cv1k_state::speedup_r()
 {
